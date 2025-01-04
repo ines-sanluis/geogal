c432c44786cba66d7c995d695631b32468e0a5e0
Ines San Luis <ines.sanluis@seven.one>
2025-01-05 00:06:22 +0100

    Update date picker

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,14 @@ export const MAX_GUESSES = 3;
 
 // Get today's date in YYYY-MM-DD format
 const getTodayString = () => {
-  return new Date().toISOString().split("T")[0];
+  const options = {
+    timeZone: "Europe/Madrid",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  };
+  const date = new Intl.DateTimeFormat("en-GB", options).format(new Date());
+  return date.split("/").reverse().join("-"); // Format as YYYY-MM-DD
 };
 
 const getDirectionArrow = (bearing) => {
